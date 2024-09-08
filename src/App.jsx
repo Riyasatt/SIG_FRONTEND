4f31fdd976ca92cb98d2c6292ff3ccd24862a864
Harshit Sachdeva <harshitsachdeva291104@gmail.com>
2024-09-08 13:25:54 +0530

    SignIn Feature Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignInForm from './components/SignInForm.jsx';  // Import your Login component
+import RegistrationForm from './components/RegistrationForm.jsx';  // Import your Registration (Sign Up) component
 
-import './App.css'
-import RegistrationForm from './components/RegistrationForm'
-
-function App() {
-
+const App = () => {
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInForm />} />  {/* Default route for Sign In */}
+        <Route path="/signup" element={<RegistrationForm />} />  {/* Route for Sign Up */}
+      </Routes>
+    </Router>
+  );
+};
 
-    <RegistrationForm />
-      
-    </>
-  )
-}
-
-export default App
+export default App;
